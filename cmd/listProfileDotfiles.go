e387440e1dd9fdc4d77ed25903e6cfc3296bd9a9
Alex Palmer <XanderXAJ@users.noreply.github.com>
2025-06-10 23:31:39 +0100

    feat: Add listProfileDotfiles subcommand (via Cobra)
    Using Cobra allows for subcommands (e.g. for structured extra
    functionality in future), organised help and automatic shell completion,
    which are all fantastic.

---
+++ cmd/listProfileDotfiles.go
@@ -0,0 +1,102 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+
+	"github.com/spf13/cobra"
+)
+
+// listProfileDotfilesCmd represents the listProfileDotfiles command
+var listProfileDotfilesCmd = &cobra.Command{
+	Use:   "listProfileDotfiles",
+	Short: "List ordered profile files in your dotfiles directories",
+	Long: `Lists ordered profile files in your dotfile directories, ready for sourcing.
+e.g. 00_system, 10_git, 99_zoxide etc.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		listDotfiles()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listProfileDotfilesCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// listProfileDotfilesCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// listProfileDotfilesCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func listDotfiles() {
+	// TODO Get environment configuration
+
+	// TODO Get shell
+	shell := "bash"
+
+	// TODO Get dotfiles basePath/matcher
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	basePaths, err := dotfileDirs(home)
+	if err != nil {
+		log.Fatalln("Error getting dotfile directories:", err)
+	}
+
+	// Discover dotfiles
+	dotfiles := make([]string, 0, 50)
+	for _, basePath := range basePaths {
+		filepath.WalkDir(basePath, func(walkPath string, d fs.DirEntry, err error) error {
+			if walkPath == basePath {
+				return nil
+			}
+			if d.IsDir() {
+				return fs.SkipDir
+			}
+			if d.Type().IsRegular() && filepath.Ext(walkPath) == "" || filepath.Ext((walkPath)) == fmt.Sprintf(".%s", shell) {
+				dotfiles = append(dotfiles, walkPath)
+			}
+			return nil
+		})
+	}
+
+	// Sort dotfiles in to order
+	sort.Slice(dotfiles, func(i, j int) bool {
+		return filepath.Base(dotfiles[i]) < filepath.Base(dotfiles[j])
+	})
+
+	// Print dotfiles
+	for _, dotfile := range dotfiles {
+		fmt.Println(dotfile)
+	}
+}
+
+func dotfileDirs(dir string) ([]string, error) {
+	var basePaths []string
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	for _, entry := range entries {
+		if entry.IsDir() {
+			matched, _ := filepath.Match(".dotfiles*", entry.Name())
+			if matched {
+				basePaths = append(basePaths, filepath.Join(dir, entry.Name(), ".bashrc.d"))
+			}
+		}
+	}
+	return basePaths, nil
+}
--- main.go
+++ main.go
@@ -1,73 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"io/fs"
-	"log"
-	"os"
-	"path/filepath"
-	"sort"
+	"github.com/xanderxaj/go-dotfiles/cmd"
 )
 
 func main() {
-	// TODO Get environment configuration
-
-	// TODO Get shell
-	shell := "bash"
-
-	// TODO Get dotfiles basePath/matcher
-	home, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	basePaths, err := dotfileDirs(home)
-	if err != nil {
-		log.Fatalln("Error getting dotfile directories:", err)
-	}
-
-	// Discover dotfiles
-	dotfiles := make([]string, 0, 50)
-	for _, basePath := range basePaths {
-		filepath.WalkDir(basePath, func(walkPath string, d fs.DirEntry, err error) error {
-			log.Println(walkPath, err)
-			if walkPath == basePath {
-				return nil
-			}
-			if d.IsDir() {
-				return fs.SkipDir
-			}
-			if d.Type().IsRegular() && filepath.Ext(walkPath) == "" || filepath.Ext((walkPath)) == fmt.Sprintf(".%s", shell) {
-				dotfiles = append(dotfiles, walkPath)
-			}
-			return nil
-		})
-	}
-
-	// Sort dotfiles in to order
-	sort.Slice(dotfiles, func(i, j int) bool {
-		return filepath.Base(dotfiles[i]) < filepath.Base(dotfiles[j])
-	})
-
-	// Print dotfiles
-	for _, dotfile := range dotfiles {
-		fmt.Println(dotfile)
-	}
-}
-
-func dotfileDirs(dir string) ([]string, error) {
-	var basePaths []string
-	entries, err := os.ReadDir(dir)
-	if err != nil {
-		return nil, err
-	}
-	for _, entry := range entries {
-		if entry.IsDir() {
-			matched, _ := filepath.Match(".dotfiles*", entry.Name())
-			if matched {
-				basePaths = append(basePaths, filepath.Join(dir, entry.Name(), ".bashrc.d"))
-			}
-		}
-	}
-	return basePaths, nil
+	cmd.Execute()
 }
